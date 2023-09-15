3c0005a329e6d92e721a8a69e48b4e20045a9590
dan13iel <130215460+dan13iel@users.noreply.github.com>
2023-09-15 13:23:08 +1000

    Added network error handling to iotawatt2chargehq.py

--- iotawatt2chargehq.py
+++ iotawatt2chargehq.py
@@ -22,27 +22,43 @@ args = my_parser.parse_args()
 # Charge HQ
 chq_url = 'https://api.chargehq.net/api/public/push-solar-data'
 
-# Getting data
-g = requests.get( "http://{}/query".format(args.ip), params = "select=[{}.watts,{}.watts]&begin=s-1m&end=s&group=all&header=no".format(args.grid, args.production), timeout=15)
+try:
+    # Getting data
+    g = requests.get( "http://{}/query".format(args.ip), params = "select=[{}.watts,{}.watts]&begin=s-1m&end=s&group=all&header=no".format(args.grid, args.production), timeout=15)
 
-#Assembling json
-data = {}
-data['apiKey'] = args.key
-if g.status_code == 200:
-   logging.info("Iotawatt collection successful")
-   data['siteMeters'] = {}
-   data['siteMeters']['production_kw'] = round( g.json()[0][1] / 1000, 3)
-   data['siteMeters']['net_import_kw'] = round( g.json()[0][0] / 1000, 3)
-   data['siteMeters']['consumption_kw'] = round( sum(g.json()[0])/1000,3)
-else:
-   data['error'] = "Collection error: " +g.reason
+    #Assembling json
+    data = {}
+    data['apiKey'] = args.key
+    if g.status_code == 200:
+       logging.info("Iotawatt collection successful")
+       data['siteMeters'] = {}
+       data['siteMeters']['production_kw'] = round( g.json()[0][1] / 1000, 3)
+       data['siteMeters']['net_import_kw'] = round( g.json()[0][0] / 1000, 3)
+       data['siteMeters']['consumption_kw'] = round( sum(g.json()[0])/1000,3)
+    else:
+       data['error'] = "Collection error: " +g.reason
 
-# Posting to CHQ
-payload = json.dumps(data)
-header = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+    # Posting to CHQ
+    payload = json.dumps(data)
+    header = {'Content-type': 'application/json', 'Accept': 'text/plain'}
 
-p = requests.post( chq_url, data=payload, headers=header)
-if p.status_code != 200:
-   logging.warning("ChargeHQ API error: " + p.reason)
-#print(payload, p.text)
-logging.info(payload, p.text)
+    p = requests.post( chq_url, data=payload, headers=header)
+    if p.status_code != 200:
+       logging.warning("ChargeHQ API error: " + p.reason)
+       error = "ChargeHQ API error: " + p.reason
+    else:
+       error = None
+    #print(payload, p.text)
+    logging.info(payload, p.text)
+    
+    return (payload, p.text, error)
+
+except requests.exceptions.Timeout:
+    logging.error("Timeout error: Request timed out")
+    return (None, None, "Timeout error: Request timed out")
+except requests.exceptions.ConnectionError as e:
+    logging.error("Connection error: {}".format(str(e)))
+    return (None, None, "Connection error: {}".format(str(e)))
+except requests.exceptions.RequestException as e:
+    logging.error("Error: " + str(e))
+    return (None, None, "Error: " + str(e)))
